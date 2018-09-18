5ccb541f3ad7ed2f2daaba2bc37d47f58cc272e1
Scott Weinstein <sweinstein27@gmail.com>
2018-09-18 14:01:32 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,6 +8,30 @@ class Song < ActiveRecord::Base
   end
 
   def genre_name
-    self.genre ? self.genre.name : nil
+    self.try(:genre).try(name)
   end
+
+  def artist_name
+    self.try(:artist).try(:name)
+  end
+  
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
+end
+
+  
 end
