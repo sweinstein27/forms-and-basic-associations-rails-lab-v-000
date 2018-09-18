b29949df29060318ac3efd50fe58bad1e9559739
Scott Weinstein <sweinstein27@gmail.com>
2018-09-18 14:00:54 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,8 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
   end
 
   def genre_name
